635f22c40bf425ad12429ad6274f1da6bbd6dc5f
ruslana <ruslana.lohvyn@gmail.com>
2025-02-27 19:37:36 +0200

    Revert "steps 1-6"
    This reverts commit 1aca7ca0233aa3b0afc429fbc186c4496af5a799.

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { setupServer } from './server.js';
-import { initMongoDB } from './db/initMongoConnection.js';
-
-const bootstrap = async () => {
-  await initMongoDB();
-  setupServer();
-};
-
-bootstrap();
