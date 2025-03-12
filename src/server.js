8c8ce018fdac32785214c868f09be39ab0670cf1
ruslana <ruslana.lohvyn@gmail.com>
2025-03-12 16:15:46 +0200

    steps 1-5

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
+
+import contactsRouter from './routes/contacts.js';
 import { getEnvVar } from './utils/getEnvVar.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -17,35 +20,11 @@ export function setupServer() {
       },
     }),
   );
+  app.use(contactsRouter);
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-    res.status(200).send({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
-  app.get('/contacts/:contactId', async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-    if (!contact) {
-      res.status(404).send({ message: 'Contact not found' });
-      return;
-    }
-    res
-      .status(200)
-      .send({
-        status: 200,
-        message: `Successfully found contact with id ${contactId}!`,
-        data: contact,
-      });
-  });
-  app.use('*', (req, res, next) => {
-    res.status(404).send({
-      message: 'Not found',
-    });
-  });
+  app.use('*', notFoundHandler);
+  app.use(errorHandler);
+
   app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
   });
