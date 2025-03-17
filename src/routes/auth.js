f614f28113880637590695313a54e48f51d2b63a
ruslana <ruslana.lohvyn@gmail.com>
2025-03-17 18:27:29 +0200

    step 4

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import { validateBody } from '../middlewares/validateBody.js';
-import { registerUserSchema } from '../validation/auth.js';
+import { loginUserSchema, registerUserSchema } from '../validation/auth.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { registerUserController } from '../controllers/auth.js';
+import {
+  loginUserController,
+  registerUserController,
+} from '../controllers/auth.js';
 
 const router = Router();
 
@@ -12,4 +15,9 @@ router.post(
   ctrlWrapper(registerUserController),
 );
 
+router.post(
+  '/login',
+  validateBody(loginUserSchema),
+  ctrlWrapper(loginUserController),
+);
 export default router;
--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import router from './routes/index.js';
 import { getEnvVar } from './utils/getEnvVar.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import cookieParser from 'cookie-parser';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -13,6 +14,7 @@ export function setupServer() {
   const app = express();
   app.use(express.json());
   app.use(cors());
+  app.use(cookieParser());
   app.use(
     pino({
       transport: {
